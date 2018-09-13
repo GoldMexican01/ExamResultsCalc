7294ef9fa3fee1d7a2dfdb4c1a25e2b7852c8a1b
Ollie P <Ollie170@outlook.com>
2018-09-13 16:04:43 +0100

    Made Working Base Version

---
+++ ExamResultsCalculator/ExamResultsCalculator/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+        Dim Result As Integer
+        Dim Grade As String
+        Console.WriteLine("Enter Exam Result (Marks out of 100)")
+        Result = Console.ReadLine()
+        Select Case Result
+            Case > 80
+                Grade = "A"
+            Case > 70
+                Grade = "B"
+            Case > 60
+                Grade = "C"
+            Case > 50
+                Grade = "D"
+            Case > 40
+                Grade = "E"
+            Case Else
+                Grade = "Fail"
+        End Select
+        Console.WriteLine("Your Mark (" & Result & ") is a " & Grade & " grade")
+        Console.ReadLine()
+    End Sub
+
+End Module
